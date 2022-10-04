cec334db81a39ce3dfd87085cfd4a821c490aecb
Leo <leo.nguyen.goldenowl@gmail.com>
2022-10-04 22:45:16 +0700

    update README

---
+++ src/database/seeds/users.sql
@@ -0,0 +1,2 @@
+INSERT INTO "public"."users" ("id", "created_at", "updated_at", "role", "email", "password", "first_name", "last_name", "bod", "gender", "phone_number") VALUES
+('e3fd96cb-7565-4fed-ab38-e6b285f0cbee', '2022-09-14 09:02:10.094892', '2022-09-14 09:02:10.094892', '1', 'admin@gmail.com', '$2b$10$pJYlNrdkJjpsS1xX3q9s1et6LBNn7cxwYWslXTmwMK069reBqqODm', 'Lorem', 'Lorem', NULL, NULL, '0300000000');
